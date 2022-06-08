d0a30ee06092705232845d85a749df3a7887d91c
4Source <38220764+4Source@users.noreply.github.com>
2022-06-08 20:43:43 +0200

    Add esp-idf-project

---
+++ esp-idf-project/main/main.c
@@ -0,0 +1,30 @@
+/**
+ * @file main.c
+ *
+ * @version v0.1.0
+ * @date 01.06.2022
+ * Homepage: github.com/4Source
+ * @copyright Apache License 2.0
+ *
+ * Copyright 2022 Dennis Möller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <esp_log.h>
+static const char *TAG = "main";
+
+void app_main(void)
+{
+
+}
